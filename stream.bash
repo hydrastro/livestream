e488bc5cce45efdcd555adb3de28e409db0b98ec
ottersarecool <51519051+ottersarecool@users.noreply.github.com>
2021-06-06 22:08:57 +0200

    changing hl_list_size to 5
    hls_list_size 5 eliminates unwanted stuttering and buffering while streaming

--- stream.bash
+++ stream.bash
@@ -14,10 +14,10 @@ TWITCHKEY="<your stream key>"
 TWITCHSRV="rtmp://fra02.contribute.live-video.net/app/"
 
 #Stream without recording
-#ffmpeg -listen 1 -i rtmp://"$IP":"$PORT"/stream/"$STREAMKEY" -c:v copy -c:a copy -flags +cgop -g "$FPS" -hls_time 2 -hls_list_size 1 -hls_allow_cache 1 -hls_flags delete_segments -flush_packets 1 stream.m3u8; \cp -f done.m3u8 stream.m3u8;
+#ffmpeg -listen 1 -i rtmp://"$IP":"$PORT"/stream/"$STREAMKEY" -c:v copy -c:a copy -flags +cgop -g "$FPS" -hls_time 2 -hls_list_size 5 -hls_allow_cache 1 -hls_flags delete_segments -flush_packets 1 stream.m3u8; \cp -f done.m3u8 stream.m3u8;
 
 #Stream with recording
-#ffmpeg -listen 1 -i rtmp://"$IP":"$PORT"/stream/"$STREAMKEY" -c:v copy -c:a copy -flags +cgop -g "$FPS" -hls_time 2 -hls_list_size 1 -hls_allow_cache 1 -hls_flags delete_segments -flush_packets 1 stream.m3u8 -f segment -c:v copy -c:a copy -strftime 1 -segment_time 1000000000 -segment_format mp4 'vods/%Y-%m-%d_%H-%M-%S_vod.mp4'; \cp -f done.m3u8 stream.m3u8;
+#ffmpeg -listen 1 -i rtmp://"$IP":"$PORT"/stream/"$STREAMKEY" -c:v copy -c:a copy -flags +cgop -g "$FPS" -hls_time 2 -hls_list_size 5 -hls_allow_cache 1 -hls_flags delete_segments -flush_packets 1 stream.m3u8 -f segment -c:v copy -c:a copy -strftime 1 -segment_time 1000000000 -segment_format mp4 'vods/%Y-%m-%d_%H-%M-%S_vod.mp4'; \cp -f done.m3u8 stream.m3u8;
 
 #Twitch relay (unencrypted)
-#ffmpeg -listen 1 -i rtmp://"$IP":6645/stream/"$STREAMKEY" -c:v copy -c:a copy -flags +cgop -g 60 -hls_time 2 -hls_list_size 1 -hls_allow_cache 1 -hls_flags delete_segments -flush_packets 1 stream.m3u8 -f flv -vcodec libx264 "$TWITCHSRV""$TWITCHKEY" -f segment -c:v copy -c:a copy -strftime 1 -segment_time 1000000000 -segment_format mp4 'vods/%Y-%m-%d_%H-%M-%S_vod.mp4'; \cp -f done.m3u8 stream.m3u8;
+#ffmpeg -listen 1 -i rtmp://"$IP":6645/stream/"$STREAMKEY" -c:v copy -c:a copy -flags +cgop -g 60 -hls_time 2 -hls_list_size 5 -hls_allow_cache 1 -hls_flags delete_segments -flush_packets 1 stream.m3u8 -f flv -vcodec libx264 "$TWITCHSRV""$TWITCHKEY" -f segment -c:v copy -c:a copy -strftime 1 -segment_time 1000000000 -segment_format mp4 'vods/%Y-%m-%d_%H-%M-%S_vod.mp4'; \cp -f done.m3u8 stream.m3u8;
