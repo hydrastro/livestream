a06af2b74ec7f5175334a544a58576b6165f9de5
hydrastro <9256943+hydrastro@users.noreply.github.com>
2021-09-11 20:07:42 +0200

    Giving the project a structure for streams handling I'm not going to make a pull request because the only purpose of my changes are for setting up a webradio stream.

--- stream.sh
+++ stream.sh
@@ -3,96 +3,112 @@
 IP="127.0.0.1"
 PORT="6645"
 STREAMKEY="stream"
-VIDEO_SOURCE="video.mp4"
-AUDIO_DIR="music"
+VIDEO_SOURCE="./video.mp4"
+AUDIO_DIR="./music"
 FONT_FILE="./fonts/FSEX302.ttf"
 TEXT_PREFIX="Now playing: "
-TEXT_SPEED="2"
-TEXT_SIZE="25"
+TEXT_SPEED="1"
+TEXT_SIZE="24"
 TEXT_BOX_COLOR="black@0.5"
-TEXT_X="30"
-TEXT_Y="30"
 TEXT_SOURCE="./text.txt"
 TEXT_BORDER_W="40"
 TEXT_BOX="1"
 QUALITY="superfast"
 FPS="30"
-VBR="1500k"
+VIDEO_BITRATE="1500k"
+AUDIO_BITRATE="128k"
 RTMP_SERVER="rtmp://${IP}:${PORT}/${STREAMKEY}"
 
 livestream_send() {
-    ffmpeg                                                                    \
-        -re                                                                   \
-        -stream_loop -1                                                       \
-        -f lavfi                                                              \
-        -i "movie=filename=${VIDEO_SOURCE}:loop=0, setpts=N/(FRAME_RATE*TB)"  \
-        -thread_queue_size 512                                                \
-        -i list.txt                                                           \
-        -map 0:v:0 -map 1:a:0                                                 \
-        -map_metadata:g 1:g                                                   \
-        -vf "drawbox=x=0:y=ih-${TEXT_BORDER_W}:color=${TEXT_BOX_COLOR}:       \
-            width=iw:height=${TEXT_BORDER_W}:t=fill,                          \
-            drawtext=fontfile=${FONT_FILE}: fontsize=${TEXT_SIZE}:            \
-            textfile=${TEXT_SOURCE}: reload=1: fontcolor=white@0.8:           \
-            x=(mod(2*n\,w+tw)-tw):y=h-line_h-10"                              \
-        -vcodec libx264                                                       \
-        -pix_fmt yuv420p                                                      \
-        -preset ${QUALITY}                                                    \
-        -r ${FPS}                                                             \
-        -g $((${FPS} * 2))                                                    \
-        -b:v ${VBR}                                                           \
-        -acodec libmp3lame                                                    \
-        -ar 44100                                                             \
-        -threads 6                                                            \
-        -qscale:v 3                                                           \
-        -b:a 320000                                                           \
-        -bufsize ${VBR}                                                       \
+    ffmpeg                                                                     \
+        -re                                                                    \
+        -stream_loop -1                                                        \
+        -f lavfi                                                               \
+        -i "movie=filename=${VIDEO_SOURCE}:loop=0, setpts=N/(FRAME_RATE*TB)"   \
+        -thread_queue_size 512                                                 \
+        -i ./list.txt                                                          \
+        -map 0:v:0 -map 1:a:0                                                  \
+        -map_metadata:g 1:g                                                    \
+        -vf "drawbox=x=0:y=ih-${TEXT_BORDER_W}:color=${TEXT_BOX_COLOR}:        \
+            width=iw:height=${TEXT_BORDER_W}:t=fill,                           \
+            drawtext=fontfile=${FONT_FILE}: fontsize=${TEXT_SIZE}:             \
+            textfile=${TEXT_SOURCE}: reload=1: fontcolor=white@0.8:            \
+            x=(mod(2*n\,w+tw)-tw):y=h-line_h-10"                               \
+        -vcodec libx264                                                        \
+        -pix_fmt yuv420p                                                       \
+        -preset ${QUALITY}                                                     \
+        -r ${FPS}                                                              \
+        -g $((${FPS} * 2))                                                     \
+        -b:v ${VIDEO_BITRATE}                                                  \
+        -acodec libmp3lame                                                     \
+        -ar 44100                                                              \
+        -threads 6                                                             \
+        -qscale:v 3                                                            \
+        -b:a ${AUDIO_BITRATE}                                                  \
+        -flush_packets 0 \
+        -bufsize 512k                                                          \
         -f flv "${RTMP_SERVER}"
 }
 #        -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
 
 livestream_listen() {
-    ffmpeg                                                                    \
-        -listen 1                                                             \
-        -timeout -1                                                           \
-        -i "$RTMP_SERVER"                                                     \
-        -c:v copy                                                             \
-        -c:a copy                                                             \
-        -flags +cgop                                                          \
-        -g "$FPS"                                                             \
-        -hls_time 2                                                           \
-        -hls_list_size 5                                                      \
-        -hls_allow_cache 1                                                    \
-        -hls_flags delete_segments                                            \
-       -flush_packets 1 stream.m3u8;                                          \
+    ffmpeg                                                                     \
+        -listen 1                                                              \
+        -timeout -1                                                            \
+        -i "$RTMP_SERVER"                                                      \
+        -c:v copy                                                              \
+        -c:a copy                                                              \
+        -flags +cgop                                                           \
+        -g "$FPS"                                                              \
+        -hls_time 2                                                            \
+        -hls_list_size 5                                                       \
+        -hls_allow_cache 1                                                     \
+        -hls_flags delete_segments                                             \
+        -flush_packets 1 stream.m3u8;                                          \
     cp -f done.m3u8 stream.m3u8;
 }
 
-livestream_send_loop() {
-    # TODO: get pid
-    livestream_send &
+livestream_manage_audio() {
+    livestream_get_random_audio
+    video_text=$(livestream_get_video_text "$RANDOM_AUDIO")
+    livestream_update_video_text "$video_text"
+    livestream_update_audio_file "$RANDOM_AUDIO" 0
+    sleep_time=$(echo|awk "{printf $AUDIO_LENGTH}")
+    current_file_id=1
     while true; do
         livestream_get_random_audio
-        livestream_update_video_text $random_audio
-        livestream_update_queue_file $random_audio
-        sleep_time=$(echo|awk "{print $audio_length - 1}")
+        livestream_update_audio_file "$RANDOM_AUDIO" $current_file_id
         sleep $sleep_time
+        sleep_time=$(echo|awk "{print $AUDIO_LENGTH}")
+        video_text=$(livestream_get_video_text "$RANDOM_AUDIO")
+        livestream_update_video_text "$video_text"
+        [[ $current_file_id == 0 ]] && current_file_id=1 || current_file_id=0
     done
 }
 
-livestream_get_random_audio() {
-    random_audio=${MUSIC[$RANDOM % ${#MUSIC[@]} ]}
-    audio_length=$(ffprobe -v error -show_entries format=duration -of \
-        default=noprint_wrappers=1:nokey=1 $random_audio)
+livestream_send_loop() {
+    livestream_manage_audio & PIDS+=( "$!" ) \
+    livestream_send & PIDS+=( "$!" )
 }
 
-livestream_update_queue_file() {
-    printf "ffconcat version 1.0\nfile '$1'\nfile list.txt" > list.txt
+livestream_get_random_audio() {
+    RANDOM_AUDIO=${MUSIC[$RANDOM % ${#MUSIC[@]} ]}
+    AUDIO_LENGTH=$(ffprobe -v error -show_entries format=duration -of \
+        default=noprint_wrappers=1:nokey=1 "$RANDOM_AUDIO")
+}
+
+livestream_update_audio_file() {
+    #ln -s -f "$1" ./audio$2.opus
+    cp -f "$1" ./audio$2.opus
+}
+
+livestream_get_video_text() {
+    audio_filename=$(basename "${1%.*}")
+    echo "$TEXT_PREFIX$audio_filename"
 }
 
 livestream_update_video_text() {
-    audio_filename=$(basename "${1%.*}")
-    echo $TEXT_PREFIX$audio_filename > ${TEXT_SOURCE}
+    echo "$1" > ${TEXT_SOURCE}
 }
 
 livestream_load_music() {
@@ -110,12 +126,19 @@ livestream_quit() {
         kill -0 "$pid" && kill "$pid"
     done
 }
+
+livestream_log() {
+    echo -n "date +"[%m-%d %H:%M:%S]" & $1" > livestream.log
 }
+
 PIDS=( )
 trap livestream_quit EXIT
 
 livestream_load_music
 
 livestream_listen & \
-sleep 2 && \
 livestream_send_loop
+
+while true; do
+    read -p "asdasd"
+done
